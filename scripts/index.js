69787f9b34bcdd1c7044e01d82cc48028ecc702f
Amilcar <amilcartoto199@gmail.com>
2024-08-28 21:07:45 -0300

    advance proyect

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,36 @@
-console.log(tempData);
+document.addEventListener('DOMContentLoaded', function() {
+    const moviesContainer = document.getElementById("movies-container");
 
+    tempData.forEach((movie) => {
+        const card = document.createElement("div");
+        card.classList.add("card");
 
-const moviesContainer = document.getElementById("movies-container");
+        const title = document.createElement("h3");
+        title.classList.add("card-title");
+        title.innerHTML = movie.title;
 
+        const year = document.createElement("p");
+        year.classList.add("card-text");
+        year.innerHTML = `Año: ${movie.year}`;
 
+        const poster = document.createElement("img");
+        poster.classList.add("card-image");
+        poster.src = movie.poster;
+        poster.alt = movie.title
 
-tempData.forEach((movie) => {
-    const card = document.createElement("div");
-    card.classList.add("card");
+        const button = document.createElement('button');
+        button.textContent = "Ver más";
+        button.onclick = function() {
+            window.location.href = movie.enlace; 
+        };
 
+        // Agrega los elementos a la tarjeta
+        card.appendChild(poster);
+        card.appendChild(title);
+        card.appendChild(year);
+        
 
-    const title = document.createElement("h3");
-    title.classList.add("card-title");
-    title.innerHTML = movie.title;
-
-    const poster = document.createElement("img");
-    postar.classList.add("card-image");
-    poster.src = movie.poster;
-
-    const year = document.createElement("p");
-    year.classList.add("card-text");
-    year.innerHTML = movie.title;
+        // Agrega la tarjeta al contenedor
+        moviesContainer.appendChild(card);
+    });
 });
--- scripts/tempData.js
+++ scripts/tempData.js
@@ -6,8 +6,8 @@ const tempData = [
         duration:" 2hr 56min",
         gender: ["Action", "Adventure", "Drama", "Fantasy"],
         rate: 8.7, 
-        poster: "https://www.google.com/imgres?q=the%20lord%20of%20the%20rings%202%20film%20poster&imgurl=https%3A%2F%2Fpostercity.com.ar%2Fwp-content%2Fuploads%2F2021%2F07%2FLord-of-the-Rings-two-towers.jpg&imgrefurl=https%3A%2F%2Fpostercity.com.ar%2Fproducto%2Fthe-lord-of-the-rings-the-two-towers%2F&docid=JA5i01e72ufKgM&tbnid=9SewtfjLXUDhGM&vet=12ahUKEwib356PqJaIAxUXrpUCHUZQN1wQM3oECB0QAA..i&w=500&h=750&hcb=2&ved=2ahUKEwib356PqJaIAxUXrpUCHUZQN1wQM3oECB0QAA",
-
+        poster: "https://www.google.com/imgres?q=poster%20del%20se%C3%B1or%20de%20los%20anillos%20la%20trilogia&imgurl=https%3A%2F%2Fhttp2.mlstatic.com%2FD_NQ_NP_961545-MLA71291510870_082023-O.webp&imgrefurl=https%3A%2F%2Farticulo.mercadolibre.com.ar%2FMLA-1483678762-3-posters-trilogia-el-senor-de-los-anillos-_JM&docid=vifgcJTgJ1ns7M&tbnid=3EA0b1Ocz7V1BM&vet=12ahUKEwiU5KHi4ZiIAxUBuJUCHf_jOZAQM3oECHkQAA..i&w=340&h=500&hcb=2&itg=1&ved=2ahUKEwiU5KHi4ZiIAxUBuJUCHf_jOZAQM3oECHkQAA",
+       
     },
     {
         title: "The Lord of the Ring: The Two Towes",
@@ -16,7 +16,7 @@ const tempData = [
         duration: "2hr 45min",
         gender:  ["Action", "Adventure", "Drama", "Fantasy"],
         rate: 7.5,
-        poster: "https://www.google.com/imgres?q=the%20lord%20of%20the%20rings%201%20film%20poster&imgurl=https%3A%2F%2Fm.media-amazon.com%2Fimages%2FM%2FMV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY%40._V1_.jpg&imgrefurl=https%3A%2F%2Fwww.imdb.com%2Ftitle%2Ftt0120737%2F&docid=EG_nUnHoVl_RiM&tbnid=6-f04EV6lrjNxM&vet=12ahUKEwjpr_LYpZaIAxXyrpUCHUTcGTAQM3oECBgQAA..i&w=1978&h=2936&hcb=2&ved=2ahUKEwjpr_LYpZaIAxXyrpUCHUTcGTAQM3oECBgQAA",
+        poster: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpostercity.com.ar%2Fproducto%2Fthe-lord-of-the-rings-the-two-towers%2F&psig=AOvVaw0rFRMyJLIdsQdjyJZggi0d&ust=1724971563659000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCLjHyoHimIgDFQAAAAAdAAAAABAE",
 
     },
     {
@@ -26,7 +26,7 @@ const tempData = [
         duration:"2hr 10min",
         gender: ["Action", "Adventure", "Drama", "Fantasy"],
         rate: 8.2,
-        poster: "https://www.google.com/imgres?q=the%20lord%20of%20the%20rings%203%20film%20poster&imgurl=https%3A%2F%2Fm.media-amazon.com%2Fimages%2FM%2FMV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM%40._V1_.jpg&imgrefurl=https%3A%2F%2Fwww.imdb.com%2Ftitle%2Ftt0167260%2F&docid=jZBx8udFimKQRM&tbnid=10vLxbpqzJRrcM&vet=12ahUKEwjdueD3pZaIAxUdqJUCHXMhDjAQM3oECBgQAA..i&w=800&h=1185&hcb=2&ved=2ahUKEwjdueD3pZaIAxUdqJUCHXMhDjAQM3oECBgQAA",
+        poster: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.filmaffinity.com%2Fes%2Ffilm226427.html&psig=AOvVaw0rFRMyJLIdsQdjyJZggi0d&ust=1724971563659000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCLjHyoHimIgDFQAAAAAdAAAAABAQ",
 
     },
     {
@@ -46,7 +46,7 @@ const tempData = [
         duration: "2hr 1min",
         gender: ["Accion", "Sci-Fic", "Adventure"],
         rate: 7.9,
-        poster: "https://www.google.com/imgres?q=back%20to%20the%20future%20II%20film%20poster&imgurl=https%3A%2F%2Fxl.movieposterdb.com%2F21_08%2F1989%2F96874%2Fxl_96874_a20cbf38.jpg&imgrefurl=https%3A%2F%2Fwww.movieposterdb.com%2Fback-to-the-future-part-ii-i96874%2Fa20cbf38&docid=muDsOdIuajHPxM&tbnid=9VjtGG3vajcemM&vet=12ahUKEwjsgeCcppaIAxWTqZUCHZBPMDsQM3oECHUQAA..i&w=600&h=900&hcb=2&ved=2ahUKEwjsgeCcppaIAxWTqZUCHZBPMDsQM3oECHUQA",
+        poster: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.movieposterdb.com%2Fback-to-the-future-part-ii-i96874%2Fa20cbf38&psig=AOvVaw2ivSW4me90g25SnmCwpcVr&ust=1724889040649000&source=images&cd=vfe&opi=89978449&ved=2ahUKEwj_tNDFrpaIAxWwPrkGHQAZNuAQjRx6BAgAEBg",
     }, 
     {
         title: "Back to the Future III",
@@ -55,7 +55,7 @@ const tempData = [
         duration: "1hr 59min",
         gender: ["Accion", "Sci-Fic", "Adventure"],
         rate: 8.6,
-        poster:"https://www.google.com/imgres?q=back%20to%20the%20future%20III%20film%20poster&imgurl=https%3A%2F%2Fm.media-amazon.com%2Fimages%2FI%2F71sPGYXUl%2BL.jpg&imgrefurl=https%3A%2F%2Fwww.amazon.com.mx%2FBack-Future-III-Movie-Poster%2Fdp%2FB0016DDVRO&docid=bpno2gponsUT7M&tbnid=_nC-te9DVo_4CM&vet=12ahUKEwjlgbupppaIAxVfpZUCHfRuOeMQM3oECBoQAA..i&w=676&h=1000&hcb=2&ved=2ahUKEwjlgbupppaIAxVfpZUCHfRuOeMQM3oECBoQAA",
+        poster:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.movieposterdb.com%2Fback-to-the-future-part-ii-i96874%2Fa20cbf38&psig=AOvVaw2ivSW4me90g25SnmCwpcVr&ust=1724889040649000&source=images&cd=vfe&opi=89978449&ved=2ahUKEwj_tNDFrpaIAxWwPrkGHQAZNuAQjRx6BAgAEBg",
 
     },
 ]
